eef4d1461a838622581ec283a3e203bd02f2a0a9
jkimber <jasonkimber@outlook.com>
2022-09-16 18:17:00 +0100

    Add supress button

--- src/App.js
+++ src/App.js
@@ -4,28 +4,41 @@ import { getData } from './data'
 import './App.css';
 
 function App() {
-  const data = getData();
+  const [data, setData] = React.useState(getData())
 
   const [item, setItem] = React.useState(data[0])
   const [toggle, setToggle] = React.useState(false)
-  const [binomialFirst, setBinomialFirst] = React.useState(false)
+  const [binomialFirst, setBinomialFirst] = React.useState(true)
+
 
   const getNext = () => {
+    const randomInteger = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+    
+    let next = data[randomInteger(0, data.length - 1)];
+    while (next[0] === item[0] && data.length > 1) {
+      next = data[randomInteger(0, data.length - 1)];
+    }
+    
     setToggle(!binomialFirst)
-    setItem(data[Math.floor((Math.random() * data.length))]);
+    setItem(next);
+  }
+
+  const supress = (supressed) => {
+    setData(data.filter(item => item[0] !== supressed))
+    getNext()
   }
 
   return (
     <div className="App">
       <main className="Main">
         <Card
-          binomial={item[0]}
-          common={item[1]}
+          binomial={data.length > 0 && data.includes(item) ? item[0] : ""}
+          common={data.length > 0 && data.includes(item) ? item[1] : [""]}
           toggle={toggle}
           onClick={() => setToggle(!toggle)} />
       </main>
-      <footer>
-        <div className='Toolbar'>
+      <footer className="Footer">
+        <div className="Toolbar">
           <label htmlFor="binomial">Binomial first
             <input
               type="radio"
@@ -48,8 +61,15 @@ function App() {
         <input
           type="button"
           className="App-button"
+          onClick={() => supress(item[0])}
+          value={`Card done (${data.length} left)`}
+          disabled={data.length === 0 || !data.includes(item)} />
+        <input
+          type="button"
+          className="App-button"
           onClick={getNext}
-          value="Next" />
+          value="Next"
+          disabled={data.length === 0 || (data.length === 1 && data.includes(item))} />
       </footer>
     </div>
   );
--- src/Card.js
+++ src/Card.js
@@ -3,5 +3,5 @@ import './Card.css'
 
 export const Card = ({ binomial, common, toggle, onClick }) =>
     <div className='Card' onClick={onClick}>
-        {toggle ? common.map(name => <p key={name}>{name}</p>) : binomial}
+        {toggle ? common.map(name => <p key={name}>{name}</p>) : <p>{binomial}</p>}
     </div>
