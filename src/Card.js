9e92e19a40f00614c2b7c1b2942b63905f599ef9
jkimber <jasonkimber@outlook.com>
2022-09-18 18:36:04 +0100

    Use Genus & species as file name. Add images

--- src/Card.js
+++ src/Card.js
@@ -3,8 +3,7 @@ import './Card.css'
 
 export const Card = ({ binomial, common, imgFilename, toggle, onClick, togglePicture }) =>
     <div className='Card' onClick={onClick}>
-        {togglePicture && imgFilename && imgFilename !== 'NULL' && <img alt={imgFilename} src={process.env.PUBLIC_URL + '/img/' + imgFilename}></img>}
-        {togglePicture && imgFilename && imgFilename === 'NULL' && <div className='Card-EmptyImage'></div>}
+        {togglePicture && <img alt={imgFilename} src={process.env.PUBLIC_URL + '/img/' + binomial.split(' ')[0] + '_' + binomial.split(' ')[1]}></img>}
         {!togglePicture && <div className='Card-EmptyImage'></div>}
         <div className='Card-Text'>
             {toggle ? common.map(name => <p key={name}>{name}</p>) : <p>{binomial}</p>}
