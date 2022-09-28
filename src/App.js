0b1022d6a16beee0e419de348e0ff2e1586ff398
jkimber <jasonkimber@outlook.com>
2022-09-28 16:21:02 +0100

    Add other modes

--- src/App.js
+++ src/App.js
@@ -1,88 +1,53 @@
-import React from 'react';
-import { Card } from './Card'
-import { getData } from './data'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+import Binomial from './Binomial'
+import Family from './Family'
+import Glossary from './Glossary'
+import Five from './Five'
+
 import './App.css';
 
-function App() {
-  const [data, setData] = React.useState(getData())
-
-  const [item, setItem] = React.useState(data[0])
-  const [toggle, setToggle] = React.useState(false)
-  const [binomialFirst, setBinomialFirst] = React.useState(true)
-  const [pictureToggle, setPictureToggle] = React.useState(true)
-
-  const getNext = () => {
-    const randomInteger = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
-    
-    let next = data[randomInteger(0, data.length - 1)];
-    while (next[0] === item[0] && data.length > 1) {
-      next = data[randomInteger(0, data.length - 1)];
-    }
-    
-    setToggle(!binomialFirst)
-    setItem(next);
-  }
-
-  const supress = (supressed) => {
-    setData(data.filter(item => item[0] !== supressed))
-    getNext()
-  }
-
-  return (
-    <div className="App">
-      <main className="Main">
-        <Card
-          binomial={data.length > 0 && data.includes(item) ? item[0] : ""}
-          common={data.length > 0 && data.includes(item) ? item[1] : [""]}
-          imgFilename={data.length > 0 && data.includes(item) ? item[2] : ""}
-          toggle={toggle}
-          togglePicture={pictureToggle}
-          onClick={() => setToggle(!toggle)} />
-      </main>
-      <footer className="Footer">
-        <div className="Toolbar">
-          <label htmlFor="binomial">Binomial
-            <input
-              type="radio"
-              name="direction"
-              value="binomial"
-              id="binomial"
-              checked={binomialFirst}
-              onChange={() => setBinomialFirst(true)} />
-          </label>
-          <label htmlFor="common">Common
-            <input
-              type="radio"
-              name="direction"
-              value="Common"
-              id="common"
-              checked={!binomialFirst}
-              onChange={() => setBinomialFirst(false)} />
-          </label>
-          <label htmlFor="common">Picture
-            <input
-              type="checkbox"
-              name="picture"
-              id="picture"
-              checked={pictureToggle}
-              onChange={() => setPictureToggle(!pictureToggle)} />
-          </label>
-        </div>
-        <input
-          type="button"
-          className="App-button"
-          onClick={() => supress(item[0])}
-          value={`Card done (${data.length} left)`}
-          disabled={data.length === 0 || !data.includes(item)} />
-        <input
-          type="button"
-          className="App-button"
-          onClick={getNext}
-          value="Next"
-          disabled={data.length === 0 || (data.length === 1 && data.includes(item))} />
-      </footer>
-    </div>
-  );
+const paths = {
+    binomial: 'binomial',
+    family: 'family',
+    glossary: 'glossary',
+    five: 'five'
 }
 
-export default App;
+const toTitle = (value) => value[0].toString().toUpperCase().concat(value.slice(1));
+
+export default function App() {
+    return (
+        <div className="App">
+            <Router>
+                <Routes>
+                    <Route path='/' element={<Menu />} />
+                    <Route path={`/${paths.binomial}`} element={<Binomial />} />
+                    <Route path={`/${paths.family}`} element={<Family />} />
+                    <Route path={`/${paths.glossary}`} element={<Glossary />} />
+                    <Route path={`/${paths.five}`} element={<Five />} />
+                </Routes>
+            </Router>
+        </div>
+    );
+}
+
+function Menu() {
+    return <nav >
+        <ul>
+            <li>
+                <Link to={`/${paths.binomial}`}>{toTitle(paths.binomial)}</Link>
+            </li>
+            <li>
+                <Link to={`/${paths.family}`}>{toTitle(paths.family)}</Link>
+            </li>
+            <li>
+                <Link to={`/${paths.glossary}`}>{toTitle(paths.glossary)}</Link>
+            </li>
+            <li>
+                <Link to={`/${paths.five}`}>{toTitle(paths.five)}</Link>
+            </li>
+        </ul>
+    </nav>
+}
